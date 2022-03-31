d4460fc98f576c0ee74ac2a8791023a63a72d703
El <qorichsan@gmail.com>
2022-03-31 07:25:42 +0700

    Search Features fix

--- src/App.js
+++ src/App.js
@@ -3,19 +3,26 @@ import data from './data'
 import './App.css'
 import Track from './Components/Tracks';
 import Wrapper from './Components/Wrapper';
+import Search from './Components/Search';
 
 function App() {
-  const clientId = process.env.CLIENT_ID
+  const access_token = window.location.hash.substring(1, window.location.hash.length -1).split("&")[0].split("=")[1]
+  
   const redirect_uri = 'http://localhost:3000/callback'
+  const clientId = '3e998473355d4fb7936636c8bd356fd6'
   const scope = 'playlist-modify-private'
-  const url = `http://accounts.spotify.com/authorize?respon_type=token&client_id=${clientId}&scope=${scope}&redirect_uri=${redirect_uri}`
+  const url = `http://accounts.spotify.com/authorize?respon_type=token&client_id=${clientId}&scope=${scope}&redirect_uri=${redirect_uri}&response_type=token&show_dialog=true`
 
   return (
+    
     <div className='wrapper'>
       <Wrapper />
-      {data.map((d)=> (
-        <Track image={d.album.images[0].url} artist={d.album.artists[0].name} title={d.name} link={d.external_urls.spotify}/>
-      ))}
+      <Search />
+      {/* {data.map((d)=> (
+        <Track key={d.id} image={d.album.images[0].url} artist={d.album.artists[0].name} title={d.name} link={d.external_urls.spotify} login={url}/>
+      ))} */}
+
+      
     </div>
   );
 }
--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Track from '../Tracks'
+import './index.css'
 
 const Search = () => {
+
+  useEffect(() => {
+    getSpotify()
+  }, [])
+
+  const accessToken = 'BQDtFi6OveMbmk71qjrpWOfoevJAhlPJzQS_cvt4m8B3y34-q8G7sM5kzJeCHwt6Wpr37bSd8Gi8FqPhyboiNqpXUzOHdK-_C0W__TL7OXit1SFvouqOlN5sPNXA2CXhB57c3nFOPI58Sa1FJXhnYYFAIs62EP3DzUvVFivO1miaxRnQ2JP_3Mt5NMie2S3EztQ'
+  const [api, setApi] = useState([])
+  const [search, setSearch] = useState('')
+  const access_token = window.location.hash.substring(1, window.location.hash.length -1).split("&")[0].split("=")[1]
+
+  const getSpotify = () => {
+    fetch(`https://api.spotify.com/v1/search?q=${search}&type=track&limit=10&access_token=${accessToken}`).then(res => res.json()).then((data) => {
+      setApi(data.tracks.items)
+    })
+    console.log(api)
+  }
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    getSpotify()
+  }
+
+
+  
+
   return (
-    <div>Search</div>
+    <div className='search'>
+
+      <input type="text" value={search} onChange={handleChange} />
+      <button type='submit' onClick={handleSubmit}>Search</button>
+      {api.map((d) => (
+        <Track key={d.id} image={d.album.images[0].url} artist={d.album.artists[0].name} title={d.name} link={d.external_urls.spotify}/>
+      ))}
+
+    </div>
   )
 }
 
