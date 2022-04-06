c36efe2516db13246edd9757455b0a4115d49252
El <qorichsan@gmail.com>
2022-04-06 15:45:07 +0700

    fix

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -4,7 +4,7 @@ const Login = () => {
 
   const access_token = window.location.hash.substring(1, window.location.hash.length -1).split("&")[0].split("=")[1]
   
-  const redirect_uri = 'http://localhost:3000/callback'
+  const redirect_uri = 'https://qorey-spotify.vercel.app/'
   const clientId = '3e998473355d4fb7936636c8bd356fd6'
   const scope = 'playlist-modify-private'
   const url = `http://accounts.spotify.com/authorize?&client_id=${clientId}&scope=${scope}&redirect_uri=${redirect_uri}&response_type=token&show_dialog=true`
