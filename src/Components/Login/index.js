0c69ad4d38214f996924eba68d9c61815e7ee961
El <qorichsan@gmail.com>
2022-04-06 15:49:37 +0700

    fixing

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -4,13 +4,13 @@ const Login = () => {
 
   const access_token = window.location.hash.substring(1, window.location.hash.length -1).split("&")[0].split("=")[1]
   
-  const redirect_uri = 'https://qorey-spotify.vercel.app/'
+  const redirectUri = 'https://qorey-spotify.vercel.app/'
   const clientId = '3e998473355d4fb7936636c8bd356fd6'
   const scope = 'playlist-modify-private'
-  const url = `http://accounts.spotify.com/authorize?&client_id=${clientId}&scope=${scope}&redirect_uri=${redirect_uri}&response_type=token&show_dialog=true`
+  const url = `http://accounts.spotify.com/authorize?&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`
 
   const handleLogin = () => {
-    window.open(`http://accounts.spotify.com/authorize?&client_id=${clientId}&scope=${scope}&redirect_uri=${redirect_uri}&response_type=token&show_dialog=true`)
+    window.open(`http://accounts.spotify.com/authorize?&client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&response_type=token&show_dialog=true`)
   }
 
   return (
